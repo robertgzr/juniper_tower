45eb53f64c6a9909bdb265f491c2f2b762fcc8a5
Robert Günzler <r@gnzler.io>
2018-10-03 17:28:10 +0200

    Initial commit of tower_web integration

---
+++ examples/tower_server.rs
@@ -0,0 +1,55 @@
+extern crate juniper;
+extern crate juniper_tower;
+extern crate tokio;
+#[macro_use]
+extern crate tower_web;
+
+use tokio::prelude::*;
+
+use juniper::tests::model::Database;
+use juniper::{EmptyMutation, RootNode};
+use juniper_tower::{GraphQLRequest, GraphQLResponse};
+use tower_web::ServiceBuilder;
+
+type Schema = RootNode<'static, Database, EmptyMutation<Database>>;
+
+struct Juniper {
+    schema: Schema,
+    context: Database, 
+}
+
+impl_web! {
+    impl Juniper {
+        #[get("/")]
+        #[content_type("text/html; charset=utf-8")]
+        fn graphiql(&self) -> impl Future<Item=String, Error=()> + Send {
+            juniper_tower::graphiql_source("/graphql")
+        }
+
+        #[get("/graphql")]
+        #[content_type("application/json")]
+        fn get_graphql_handler(&self, query_string: GraphQLRequest) -> impl Future<Item=GraphQLResponse, Error=()> + Send {
+            query_string.execute(&self.schema, &self.context)
+        }
+
+        #[post("/graphql")]
+        #[content_type("application/json")]
+        fn post_graphql_handler(&self, body: GraphQLRequest) -> impl Future<Item=GraphQLResponse, Error=()> + Send {
+            body.execute(&self.schema, &self.context)
+        }
+    }
+}
+
+fn main() {
+    let addr = "127.0.0.1:8080".parse().expect("invalid address");
+    println!("running: {}", addr);
+    ServiceBuilder::new()
+        .resource(Juniper {
+            schema: Schema::new(
+                      Database::new(),
+                      EmptyMutation::<Database>::new()),
+            context: Database::new(),
+        })
+        .run(&addr)
+        .unwrap();
+}
---
+++ src/lib.rs
@@ -0,0 +1,55 @@
+extern crate juniper;
+extern crate http;
+extern crate serde_json;
+extern crate tokio;
+#[macro_use]
+extern crate tower_web;
+
+use http::StatusCode;
+use tokio::prelude::*;
+
+use juniper::GraphQLType;
+use juniper::RootNode;
+
+#[derive(Debug, Extract, Deserialize)]
+pub struct GraphQLRequest {
+    #[serde(flatten)]
+    gq: juniper::http::GraphQLRequest
+}
+
+#[derive(Debug, Response, Serialize)]
+pub struct GraphQLResponse {
+    #[serde(flatten)]
+    inner: serde_json::Value,
+
+    #[web(header)]
+    status: u16,
+}
+
+pub fn graphiql_source(graphql_endpoint_url: &str) -> impl Future<Item = String, Error = ()> + Send {
+    future::ok(juniper::graphiql::graphiql_source(graphql_endpoint_url))
+}
+
+impl GraphQLRequest {
+    pub fn execute<CtxT, QueryT, MutationT>(
+        &self, 
+        root_node: &RootNode<QueryT, MutationT>,
+        context: &CtxT,
+    ) -> impl Future<Item=GraphQLResponse, Error=()>
+    where
+        QueryT: GraphQLType<Context = CtxT>,
+        MutationT: GraphQLType<Context = CtxT>,
+    {
+        let response = self.gq.execute(root_node, context);
+        let status = if response.is_ok() {
+            StatusCode::OK
+        } else {
+            StatusCode::BAD_REQUEST
+        };
+        let json = serde_json::to_value(&response).unwrap();
+        future::ok(GraphQLResponse {
+            inner: json,
+            status: status.into(),
+        })
+    }
+}
